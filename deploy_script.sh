d26759234bfb0286b0a7c24b44e734dea938c2c8
Errietta Kostala <errietta@errietta.me>
2019-02-03 16:59:08 +0000

    fix

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,7 @@
  if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&
     [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     pip install poetry
-    poetry publish
+    poetry publish --build
   else
     echo "Skipping deployment, as this is not a tagged commit"
   fi
