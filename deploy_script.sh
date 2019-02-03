4af7d9472ed58518b97d07da2e5be21e074a837a
Errietta Kostala <errietta@errietta.me>
2019-02-03 19:20:41 +0000

    debug

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,8 @@
- if ([ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ]) &&
+echo $TRAVIS_BRANCH 
+echo $TRAVIS_PULL_REQUEST
+echo $TRAVIS_TAG
+
+if ([ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ]) &&
     [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     pip install poetry
     poetry publish --build -u $USERNAME -p $PASSWORD
