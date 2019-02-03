5f2a9abf26267a04cb58238abd9cdf7997949de7
Errietta Kostala <errietta@errietta.me>
2019-02-03 19:23:55 +0000

    apparently i'm bad at bash scripting

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,13 +1,9 @@
-echo $TRAVIS_BRANCH 
-echo $TRAVIS_PULL_REQUEST
-echo $TRAVIS_TAG
 
-if ([ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ]) &&
-    [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     pip install poetry
     poetry publish --build -u $USERNAME -p $PASSWORD
-  else
-    echo "Skipping deployment, as this is not a tagged commit"
-  fi
+else
+  echo "Skipping deployment, as this is not a tagged commit"
+fi
 
 
